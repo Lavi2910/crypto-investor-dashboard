75c4ba37869168b00356b7a745fbcf38b50be30e
Lavi2910 <almog.lavi29@gmail.com>
2026-06-18 17:29:54 +0300

    add register endpoint with hashing and JWT

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./db";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoutes);
 
 app.get("/health", (req: Request, res: Response) => {
   res.json({ ok: true });
